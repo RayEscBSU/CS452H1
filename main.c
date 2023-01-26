fe19335b09554e5a76920b9fb90eed57c93ca5e6
RayEscBSU <raymundoescobedo@u.boisestate.edu>
2023-01-25 21:39:47 -0700

    test done

--- main.c
+++ main.c
@@ -3,6 +3,97 @@
 
 #include "deq.h"
 
+
+
+/**@Author: Raymundo escobedo
+ * CS 452
+ * Spring 2023
+*/
+
+/**
+ * run test for rem  
+ * Deq - q 
+ * ht - 0 = deq_head_put, 1 = deq_tail_put 
+ * r - 0 = deq_head_ith, 1 = deq_tail_ith
+ * 
+*/
+static void run_rem (Deq q,int ht, int r){
+  char *t1 = "test1 ";
+  char *t2 = "test2 ";
+  char *t3 = "test3 "; 
+  char *t4 = "test4 ";
+  char *t5 = "test5 ";
+  char *t6 = "test6 ";
+  char *t7 = "test7 "; 
+  
+  if (ht == 0){
+  printf("*****PUT HEAD *****\n");
+  
+  deq_head_put(q, t1);
+  deq_head_put(q, t2);
+  deq_head_put(q, t3);
+  deq_head_put(q, t4);
+  deq_head_put(q, t5);
+  deq_head_put(q, t6);
+  deq_head_put(q, t7);
+
+  }
+  else if(ht == 1 ){
+  printf("*****PUT TAIL *****\n");
+  
+  deq_tail_put(q,t1); 
+  deq_tail_put(q,t2);
+  deq_tail_put(q,t3);
+  deq_tail_put(q,t4); 
+  deq_tail_put(q,t5);
+  deq_tail_put(q,t6);
+  deq_tail_put(q,t7);
+
+  }
+  else {
+    printf("An invalid option was entered for ht\n");
+    return ; 
+  }
+  char *s=deq_str(q,0);
+  printf("%s\n",s);
+  free(s);
+
+
+  
+  
+  if(r == 0){
+    char *x; 
+    printf("*****REM Head *****\n");
+    x =deq_head_rem(q,t2); 
+    printf("%s\n",x);
+    //free(x); 
+    printf("*****NEW LIST*****\n");
+    
+    char *s=deq_str(q,0);
+    printf("%s\n",s);
+   free(s);
+  }
+  else if (r == 1)
+  {
+    char *y; 
+    printf("*****REM Tail *****\n");
+    y =deq_tail_rem(q, t3); 
+    printf("%s\n",y);
+    //free(y); 
+    printf("*****NEW LIST*****\n");
+
+   char  *s=deq_str(q,0);
+    printf("%s\n",s);
+    free(s);
+  }
+  else {
+    printf("An invalid option was entered for r \n");
+    return ; 
+  }
+
+}
+
+
 /**
  * run test for ith 
  * Deq - q 
@@ -148,8 +239,10 @@ static void run_ith(Deq q,int ht, int r, int index){
 int main() {
   Deq q=deq_new();
 
- // run_test(q,5);
+  // run_test(q,5);
   //run_ith(q,0, 0, 2);
+  run_rem(q,0,1);
+  run_rem(q,1,0);
 
   deq_del(q,0);
   return 0;
