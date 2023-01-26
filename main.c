ed04f8aa0ad0f0fc864282c914a0263c58967a5b
RayEscBSU <raymundoescobedo@u.boisestate.edu>
2023-01-26 08:40:17 -0700

    i think it works

--- main.c
+++ main.c
@@ -177,73 +177,73 @@
  * r - 0 = deq_head_ith, 1 = deq_tail_ith
  * index - specified index to remove
 */
-static void run_ith(Deq q,int ht, int r, int index){
-  char *t1 = "test1 ";
-  char *t2 = "test2 ";
-  char *t3 = "test3 ";
-  char *t4 = "test4 ";
-  char *t5 = "test5 ";
-  char *t6 = "test6 ";
-  char *t7 = "test7 ";
+// static void run_ith(Deq q,int ht, int r, int index){
+//   char *t1 = "test1 ";
+//   char *t2 = "test2 ";
+//   char *t3 = "test3 ";
+//   char *t4 = "test4 ";
+//   char *t5 = "test5 ";
+//   char *t6 = "test6 ";
+//   char *t7 = "test7 ";
 
-  if (ht == 0){
-  printf("\nPUT HEAD:\n");
+//   if (ht == 0){
+//   printf("\nPUT HEAD:\n");
 
-  deq_head_put(q, t1);
-  deq_head_put(q, t2);
-  deq_head_put(q, t3);
-  deq_head_put(q, t4);
-  deq_head_put(q, t5);
-  deq_head_put(q, t6);
-  deq_head_put(q, t7);
+//   deq_head_put(q, t1);
+//   deq_head_put(q, t2);
+//   deq_head_put(q, t3);
+//   deq_head_put(q, t4);
+//   deq_head_put(q, t5);
+//   deq_head_put(q, t6);
+//   deq_head_put(q, t7);
 
-  }
-  else if(ht == 1 ){
-  printf("\nPUT TAIL:\n");
+//   }
+//   else if(ht == 1 ){
+//   printf("\nPUT TAIL:\n");
 
-  deq_tail_put(q,t1);
-  deq_tail_put(q,t2);
-  deq_tail_put(q,t3);
-  deq_tail_put(q,t4);
-  deq_tail_put(q,t5);
-  deq_tail_put(q,t6);
-  deq_tail_put(q,t7);
+//   deq_tail_put(q,t1);
+//   deq_tail_put(q,t2);
+//   deq_tail_put(q,t3);
+//   deq_tail_put(q,t4);
+//   deq_tail_put(q,t5);
+//   deq_tail_put(q,t6);
+//   deq_tail_put(q,t7);
 
-  }else {
-    printf("An invalid option was entered for ht\n");
-    return ;
-  }
+//   }else {
+//     printf("An invalid option was entered for ht\n");
+//     return ;
+//   }
 
-  char *s=deq_str(q,0);
-  printf("%s\n",s);
-  free(s);
-  printf("Index to return: %d \n", index);
+//   char *s=deq_str(q,0);
+//   printf("%s\n",s);
+//   free(s);
+//   printf("Index to return: %d \n", index);
 
-  if(r == 0){
-    printf("iTH Head:\n");
-    s =deq_head_ith(q,index);
-    printf("%s\n",s);
-  }
-  else if (r == 1)
-  {
-    printf("iTH Tail:\n");
-    s =deq_tail_ith(q, index);
-    printf("%s\n",s);
-  }
-  else {
-    printf("An invalid option was entered for r \n");
-    return ;
-  }
-  printf("\n***Test complete*****\n");
+//   if(r == 0){
+//     printf("iTH Head:\n");
+//     s =deq_head_ith(q,index);
+//     printf("%s\n",s);
+//   }
+//   else if (r == 1)
+//   {
+//     printf("iTH Tail:\n");
+//     s =deq_tail_ith(q, index);
+//     printf("%s\n",s);
+//   }
+//   else {
+//     printf("An invalid option was entered for r \n");
+//     return ;
+//   }
+//   printf("\n***Test complete*****\n");
 
-}
+// }
 
 int main()
 {
   Deq q = deq_new();
 
    //run_put_get(q,0, 1);
-   run_ith(q,0, 1, 3);
+   //run_ith(q,0, 1, 3);
    //run_rem(q,0,1);
    //run_rem(q,1,0);
 
