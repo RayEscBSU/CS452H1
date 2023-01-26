2c6dd44a749bd57ee61e34556bb551e62aa8fcba
RayEscBSU <raymundoescobedo@u.boisestate.edu>
2023-01-25 21:51:50 -0700

    format

--- deq.c
+++ deq.c
@@ -6,229 +6,260 @@
 #include "error.h"
 
 // indices and size of array of node pointers
-typedef enum {Head,Tail,Ends} End;
+typedef enum
+{
+  Head,
+  Tail,
+  Ends
+} End;
 
-typedef struct Node {
-  struct Node *np[Ends];		// next/prev neighbors
+typedef struct Node
+{
+  struct Node *np[Ends]; // next/prev neighbors
   Data data;
 } *Node;
 
-typedef struct {
-  Node ht[Ends];			// head/tail nodes
+typedef struct
+{
+  Node ht[Ends]; // head/tail nodes
   int len;
 } *Rep;
 
-static Rep rep(Deq q) {
-  if (!q) ERROR("zero pointer");
+static Rep rep(Deq q)
+{
+  if (!q)
+    ERROR("zero pointer");
   return (Rep)q;
 }
 
 /**
  * Put - adds a node to the end of the list
- * H n n n T 
- * head_put - nodes is added on the left end 
+ * H n n n T
+ * head_put - nodes is added on the left end
  * tail_put - nodes are added on the right end
-*/
-static void put(Rep r, End e, Data d) {
-    struct Node *newN = (struct Node*) malloc(sizeof (struct Node) + sizeof(d));
-    newN -> data = d;
-    int h = Tail; 
-    int t = Head; 
+ */
+static void put(Rep r, End e, Data d)
+{
+  struct Node *newN = (struct Node *)malloc(sizeof(struct Node) + sizeof(d));
+  newN->data = d;
+  int h = Tail;
+  int t = Head;
 
-    if(e == Tail){ //put starting at tail, revers H and T values
-      h  = Head; 
-      t = Tail; 
-    }
+  if (e == Tail)
+  { // put starting at tail, revers H and T values
+    h = Head;
+    t = Tail;
+  }
 
-    if ( r->len == 0){  // if list is empty
-      r->ht[Head] = newN; //set new node to h and t
-      r->ht[Tail] = newN;
-      newN -> np[1] = NULL;
-      newN -> np[0] = NULL;
-    }
-    else{
+  if (r->len == 0)
+  {                     // if list is empty
+    r->ht[Head] = newN; // set new node to h and t
+    r->ht[Tail] = newN;
+    newN->np[1] = NULL;
+    newN->np[0] = NULL;
+  }
+  else
+  {
     //     list is not empty
-      struct Node *temp= r->ht[t];
-      temp->np[t] = newN;
-      newN -> np[h] = temp;
-      newN -> np[t] = NULL;
-      r->ht[t] = newN;  
-    }
+    struct Node *temp = r->ht[t];
+    temp->np[t] = newN;
+    newN->np[h] = temp;
+    newN->np[t] = NULL;
+    r->ht[t] = newN;
+  }
 
-    r->len += 1;
-    return ;
+  r->len += 1;
+  return;
 }
-
 
 /**
  * ith - returns data at a specified index depnding on direction
- *  H n n n T 
- * head_ith - retuns the node on the left end 
+ *  H n n n T
+ * head_ith - retuns the node on the left end
  * tail_ith - returns the added on the right end
- * Reutrns - node data 
-*/
-static Data ith(Rep r, End e, int i)  {
-  if (i < 0 || i > r->len){
+ * Reutrns - node data
+ */
+static Data ith(Rep r, End e, int i)
+{
+  if (i < 0 || i > r->len)
+  {
     printf("Index out of bounds\n");
     printf("Integer value given is not a valid index\n");
-    return NULL; 
+    return NULL;
   }
 
-  int c =0; 
+  int c = 0;
 
-  int h = Head; 
-  int t = Tail; 
-  if(e == Tail){ //put starting at tail, revers H and T values
-    h  = Tail; 
-    t = Head; 
+  int h = Head;
+  int t = Tail;
+  if (e == Tail)
+  { // put starting at tail, revers H and T values
+    h = Tail;
+    t = Head;
   }
 
-  struct Node *temp = r->ht[h]; 
-  
-  while(temp){
-    if(c == i ){
-      return temp->data; 
+  struct Node *temp = r->ht[h];
+
+  while (temp)
+  {
+    if (c == i)
+    {
+      return temp->data;
     }
 
     temp = temp->np[t];
     c++;
-    
   }
 
   printf("*****Node Does Not Exist*****\n");
-  return NULL; 
+  return NULL;
 }
 
 /**
  * Get - Returns the data from the last node
- * * H n n n T 
- * deq_head_get - node on the left end is returned 
- * deq_tail_get - node on the right end is returned 
- * Reutrns - node data 
-*/
-static Data get(Rep r, End e)         {
-  if (r->len == 0 ){
+ * * H n n n T
+ * deq_head_get - node on the left end is returned
+ * deq_tail_get - node on the right end is returned
+ * Reutrns - node data
+ */
+static Data get(Rep r, End e)
+{
+  if (r->len == 0)
+  {
     printf("*****Empy list*****\n");
-    return 0; 
+    return 0;
   }
-  int h = Tail; 
-  int t = Head; 
-  if(e == Tail){ //put starting at tail, revers H and T values
-    h  = Head; 
-    t = Tail; 
+  int h = Tail;
+  int t = Head;
+  if (e == Tail)
+  { // put starting at tail, revers H and T values
+    h = Head;
+    t = Tail;
   }
-  
-  struct Node *oT= r->ht[t]; 
-  struct Node *nT = oT->np[h]; 
 
-  oT->np[h] = NULL; 
-  nT->np[t] = NULL; 
+  struct Node *oT = r->ht[t];
+  struct Node *nT = oT->np[h];
 
-  r->ht[t] = nT; 
-  r-> len--; 
-  Data ret = oT->data; 
-  free(oT); 
+  oT->np[h] = NULL;
+  nT->np[t] = NULL;
+
+  r->ht[t] = nT;
+  r->len--;
+  Data ret = oT->data;
+  free(oT);
   return ret;
-
 }
 
 /**
- * Rem - removes the node with matching data then removes node 
+ * Rem - removes the node with matching data then removes node
  * Returns - mathcing data from list
- * 
-*/
-static Data rem(Rep r, End e, Data d) {
-  struct Node *curr = r->ht[Head]; 
-  struct Node *tempH; //store values of head and node from curr
-  struct Node *tempT; 
+ *
+ */
+static Data rem(Rep r, End e, Data d)
+{
+  struct Node *curr = r->ht[Head];
+  struct Node *tempH; // store values of head and node from curr
+  struct Node *tempT;
 
-  while(curr){
-    if(curr->data == d){
-      //rem begining 
-      if(curr == r->ht[Head]){
-        return get(r, Head); 
+  while (curr)
+  {
+    if (curr->data == d)
+    {
+      // rem begining
+      if (curr == r->ht[Head])
+      {
+        return get(r, Head);
       }
-      else if(curr == r->ht[Tail]){ // rem from end 
-        return get(r,Tail);
+      else if (curr == r->ht[Tail])
+      { // rem from end
+        return get(r, Tail);
       }
-      else {
-      //rem middle 
-      tempH = curr->np[Head];
-      tempT = curr->np[Tail];
+      else
+      {
+        // rem middle
+        tempH = curr->np[Head];
+        tempT = curr->np[Tail];
 
-      tempH->np[Tail] = NULL; 
-      tempT->np[Head] = NULL;
-      
-      tempH->np[Tail] = tempT; 
-      tempT->np[Head] = tempH; 
+        tempH->np[Tail] = NULL;
+        tempT->np[Head] = NULL;
 
-      curr->np[Head] = NULL; 
-      curr->np[Tail] = NULL; 
+        tempH->np[Tail] = tempT;
+        tempT->np[Head] = tempH;
 
-      r->len --;
-      Data ret = curr->data; 
-      free(curr);  
-      return ret; 
+        curr->np[Head] = NULL;
+        curr->np[Tail] = NULL;
+
+        r->len--;
+        Data ret = curr->data;
+        free(curr);
+        return ret;
       }
     }
     else
-    { //contine to next node 
+    { // contine to next node
       curr = curr->np[Tail];
     }
   }
 
   printf("*****Node Does Not Exist*****\n");
-  return NULL; 
-    
+  return NULL;
 }
 
-
-
-extern Deq deq_new() {
-  Rep r=(Rep)malloc(sizeof(*r));
-  if (!r) ERROR("malloc() failed");
-  r->ht[Head]=0;
-  r->ht[Tail]=0;
-  r->len=0;
+extern Deq deq_new()
+{
+  Rep r = (Rep)malloc(sizeof(*r));
+  if (!r)
+    ERROR("malloc() failed");
+  r->ht[Head] = 0;
+  r->ht[Tail] = 0;
+  r->len = 0;
   return r;
 }
 
-
 extern int deq_len(Deq q) { return rep(q)->len; }
 
-extern void deq_head_put(Deq q, Data d) {        put(rep(q),Head,d); }
-extern Data deq_head_get(Deq q)         { return get(rep(q),Head); }
-extern Data deq_head_ith(Deq q, int i)  { return ith(rep(q),Head,i); }
-extern Data deq_head_rem(Deq q, Data d) { return rem(rep(q),Head,d); }
+extern void deq_head_put(Deq q, Data d) { put(rep(q), Head, d); }
+extern Data deq_head_get(Deq q) { return get(rep(q), Head); }
+extern Data deq_head_ith(Deq q, int i) { return ith(rep(q), Head, i); }
+extern Data deq_head_rem(Deq q, Data d) { return rem(rep(q), Head, d); }
 
-extern void deq_tail_put(Deq q, Data d) {        put(rep(q),Tail,d); }
-extern Data deq_tail_get(Deq q)         { return get(rep(q),Tail); }
-extern Data deq_tail_ith(Deq q, int i)  { return ith(rep(q),Tail,i); }
-extern Data deq_tail_rem(Deq q, Data d) { return rem(rep(q),Tail,d); }
+extern void deq_tail_put(Deq q, Data d) { put(rep(q), Tail, d); }
+extern Data deq_tail_get(Deq q) { return get(rep(q), Tail); }
+extern Data deq_tail_ith(Deq q, int i) { return ith(rep(q), Tail, i); }
+extern Data deq_tail_rem(Deq q, Data d) { return rem(rep(q), Tail, d); }
 
-extern void deq_map(Deq q, DeqMapF f) {
-  for (Node n=rep(q)->ht[Head]; n; n=n->np[Tail])
+extern void deq_map(Deq q, DeqMapF f)
+{
+  for (Node n = rep(q)->ht[Head]; n; n = n->np[Tail])
     f(n->data);
 }
 
-extern void deq_del(Deq q, DeqMapF f) {
-  if (f) deq_map(q,f);
-  Node curr=rep(q)->ht[Head];
-  while (curr) {
-    Node next=curr->np[Tail];
+extern void deq_del(Deq q, DeqMapF f)
+{
+  if (f)
+    deq_map(q, f);
+  Node curr = rep(q)->ht[Head];
+  while (curr)
+  {
+    Node next = curr->np[Tail];
     free(curr);
-    curr=next;
+    curr = next;
   }
   free(q);
 }
 
-extern Str deq_str(Deq q, DeqStrF f) {
-  char *s=strdup("");
-  for (Node n=rep(q)->ht[Head]; n; n=n->np[Tail]) {
-    char *d=f ? f(n->data) : n->data;
-    char *t; asprintf(&t,"%s%s%s",s,(*s ? " " : ""),d);
-    free(s); s=t;
-    if (f) free(d);
+extern Str deq_str(Deq q, DeqStrF f)
+{
+  char *s = strdup("");
+  for (Node n = rep(q)->ht[Head]; n; n = n->np[Tail])
+  {
+    char *d = f ? f(n->data) : n->data;
+    char *t;
+    asprintf(&t, "%s%s%s", s, (*s ? " " : ""), d);
+    free(s);
+    s = t;
+    if (f)
+      free(d);
   }
   return s;
 }
--- main.c
+++ main.c
@@ -3,248 +3,236 @@
 
 #include "deq.h"
 
-
-
 /**@Author: Raymundo escobedo
  * CS 452
  * Spring 2023
-*/
+ */
 
 /**
- * run test for rem  
- * Deq - q 
- * ht - 0 = deq_head_put, 1 = deq_tail_put 
+ * run test for rem
+ * Deq - q
+ * ht - 0 = deq_head_put, 1 = deq_tail_put
  * r - 0 = deq_head_ith, 1 = deq_tail_ith
- * 
-*/
-static void run_rem (Deq q,int ht, int r){
-  char *t1 = "test1 ";
-  char *t2 = "test2 ";
-  char *t3 = "test3 "; 
-  char *t4 = "test4 ";
-  char *t5 = "test5 ";
-  char *t6 = "test6 ";
-  char *t7 = "test7 "; 
-  
-  if (ht == 0){
-  printf("*****PUT HEAD *****\n");
-  
-  deq_head_put(q, t1);
-  deq_head_put(q, t2);
-  deq_head_put(q, t3);
-  deq_head_put(q, t4);
-  deq_head_put(q, t5);
-  deq_head_put(q, t6);
-  deq_head_put(q, t7);
+ *
+ */
+// static void run_rem (Deq q,int ht, int r){
+//   char *t1 = "test1 ";
+//   char *t2 = "test2 ";
+//   char *t3 = "test3 ";
+//   char *t4 = "test4 ";
+//   char *t5 = "test5 ";
+//   char *t6 = "test6 ";
+//   char *t7 = "test7 ";
 
-  }
-  else if(ht == 1 ){
-  printf("*****PUT TAIL *****\n");
-  
-  deq_tail_put(q,t1); 
-  deq_tail_put(q,t2);
-  deq_tail_put(q,t3);
-  deq_tail_put(q,t4); 
-  deq_tail_put(q,t5);
-  deq_tail_put(q,t6);
-  deq_tail_put(q,t7);
+//   if (ht == 0){
+//   printf("*****PUT HEAD *****\n");
 
-  }
-  else {
-    printf("An invalid option was entered for ht\n");
-    return ; 
-  }
-  char *s=deq_str(q,0);
-  printf("%s\n",s);
-  free(s);
+//   deq_head_put(q, t1);
+//   deq_head_put(q, t2);
+//   deq_head_put(q, t3);
+//   deq_head_put(q, t4);
+//   deq_head_put(q, t5);
+//   deq_head_put(q, t6);
+//   deq_head_put(q, t7);
 
+//   }
+//   else if(ht == 1 ){
+//   printf("*****PUT TAIL *****\n");
 
-  
-  
-  if(r == 0){
-    char *x; 
-    printf("*****REM Head *****\n");
-    x =deq_head_rem(q,t2); 
-    printf("%s\n",x);
-    //free(x); 
-    printf("*****NEW LIST*****\n");
-    
-    char *s=deq_str(q,0);
-    printf("%s\n",s);
-   free(s);
-  }
-  else if (r == 1)
-  {
-    char *y; 
-    printf("*****REM Tail *****\n");
-    y =deq_tail_rem(q, t3); 
-    printf("%s\n",y);
-    //free(y); 
-    printf("*****NEW LIST*****\n");
+//   deq_tail_put(q,t1);
+//   deq_tail_put(q,t2);
+//   deq_tail_put(q,t3);
+//   deq_tail_put(q,t4);
+//   deq_tail_put(q,t5);
+//   deq_tail_put(q,t6);
+//   deq_tail_put(q,t7);
 
-   char  *s=deq_str(q,0);
-    printf("%s\n",s);
-    free(s);
-  }
-  else {
-    printf("An invalid option was entered for r \n");
-    return ; 
-  }
-  printf("*****Test complete*****\n\n");
+//   }
+//   else {
+//     printf("An invalid option was entered for ht\n");
+//     return ;
+//   }
+//   char *s=deq_str(q,0);
+//   printf("%s\n",s);
+//   free(s);
 
+//   if(r == 0){
+//     char *x;
+//     printf("*****REM Head *****\n");
+//     x =deq_head_rem(q,t2);
+//     printf("%s\n",x);
+//     //free(x);
+//     printf("*****NEW LIST*****\n");
 
-}
+//     char *s=deq_str(q,0);
+//     printf("%s\n",s);
+//    free(s);
+//   }
+//   else if (r == 1)
+//   {
+//     char *y;
+//     printf("*****REM Tail *****\n");
+//     y =deq_tail_rem(q, t3);
+//     printf("%s\n",y);
+//     //free(y);
+//     printf("*****NEW LIST*****\n");
 
+//    char  *s=deq_str(q,0);
+//     printf("%s\n",s);
+//     free(s);
+//   }
+//   else {
+//     printf("An invalid option was entered for r \n");
+//     return ;
+//   }
+//   printf("*****Test complete*****\n\n");
 
-/**
- * run test for ith 
- * Deq - q 
- * ht - 0 = deq_head_put, 1 = deq_tail_put 
- * r - 0 = deq_head_ith, 1 = deq_tail_ith
- * index - index to remove 
-*/
-static void run_ith(Deq q,int ht, int r, int index){
-  char *t1 = "test1 ";
-  char *t2 = "test2 ";
-  char *t3 = "test3 "; 
-  char *t4 = "test4 ";
-  char *t5 = "test5 ";
-  char *t6 = "test6 ";
-  char *t7 = "test7 "; 
+// }
 
-  if (ht == 0){
-  printf("*****PUT HEAD *****\n");
-  
-  deq_head_put(q, t1);
-  deq_head_put(q, t2);
-  deq_head_put(q, t3);
-  deq_head_put(q, t4);
-  deq_head_put(q, t5);
-  deq_head_put(q, t6);
-  deq_head_put(q, t7);
+// /**
+//  * run test for ith
+//  * Deq - q
+//  * ht - 0 = deq_head_put, 1 = deq_tail_put
+//  * r - 0 = deq_head_ith, 1 = deq_tail_ith
+//  * index - index to remove
+// */
+// static void run_ith(Deq q,int ht, int r, int index){
+//   char *t1 = "test1 ";
+//   char *t2 = "test2 ";
+//   char *t3 = "test3 ";
+//   char *t4 = "test4 ";
+//   char *t5 = "test5 ";
+//   char *t6 = "test6 ";
+//   char *t7 = "test7 ";
 
-  }
-  else if(ht == 1 ){
-  printf("*****PUT TAIL *****\n");
-  
-  deq_tail_put(q,t1); 
-  deq_tail_put(q,t2);
-  deq_tail_put(q,t3);
-  deq_tail_put(q,t4); 
-  deq_tail_put(q,t5);
-  deq_tail_put(q,t6);
-  deq_tail_put(q,t7);
+//   if (ht == 0){
+//   printf("*****PUT HEAD *****\n");
 
-  }else {
-    printf("An invalid option was entered for ht\n");
-    return ; 
-  }
+//   deq_head_put(q, t1);
+//   deq_head_put(q, t2);
+//   deq_head_put(q, t3);
+//   deq_head_put(q, t4);
+//   deq_head_put(q, t5);
+//   deq_head_put(q, t6);
+//   deq_head_put(q, t7);
 
-  char *s=deq_str(q,0);
-  printf("%s\n",s);
-  free(s);
-  printf("Index to remove: %d \n", index); 
+//   }
+//   else if(ht == 1 ){
+//   printf("*****PUT TAIL *****\n");
 
-  if(r == 0){
-    printf("*****iTH Head *****\n");
-    s =deq_head_ith(q,index); 
-    printf("%s\n",s);
-  }
-  else if (r == 1)
-  {
-    printf("*****iTH Tail *****\n");
-    s =deq_tail_ith(q, index); 
-    printf("%s\n",s);
-  }
-  else {
-    printf("An invalid option was entered for r \n");
-    return ; 
-  }
-  printf("*****Test complete*****\n");
- 
-}
+//   deq_tail_put(q,t1);
+//   deq_tail_put(q,t2);
+//   deq_tail_put(q,t3);
+//   deq_tail_put(q,t4);
+//   deq_tail_put(q,t5);
+//   deq_tail_put(q,t6);
+//   deq_tail_put(q,t7);
 
-/**
- * run_test - test put get 
- * t - test to run ( 1 = head put, 2 - tail put, 3 = tail put/get head, 
- *    4 - tail put/get tail )
-*/
+//   }else {
+//     printf("An invalid option was entered for ht\n");
+//     return ;
+//   }
 
-static void run_test(Deq q, int t){
-  
-  char *t1 = "test1 ";
-  char *t2 = "test2 ";
-  char *t3 = "test3 "; 
-  char *t4 = "test4 ";
-  char *t5 = "test5 ";
-  char *t6 = "test6 ";
-  char *t7 = "test7 "; 
+//   char *s=deq_str(q,0);
+//   printf("%s\n",s);
+//   free(s);
+//   printf("Index to remove: %d \n", index);
 
-  if (t == 1){
-    printf("*****PUT H *****\n");
-    deq_head_put(q, t1);
-    deq_head_put(q, t2);
-    deq_head_put(q, t3); 
-    deq_head_put(q, t4);
-    deq_head_put(q, t5);
-    deq_head_put(q, t6);
-    deq_head_put(q, t7);
-  }
-  else if (t == 2){
-    
-   printf("*****PUT T *****\n");
-    deq_tail_put(q, t1);
-    deq_tail_put(q, t2);
-    deq_tail_put(q, t3); 
-    deq_tail_put(q, t4);
-    deq_tail_put(q, t5);
-    deq_tail_put(q, t6);
-    deq_tail_put(q, t7);
+//   if(r == 0){
+//     printf("*****iTH Head *****\n");
+//     s =deq_head_ith(q,index);
+//     printf("%s\n",s);
+//   }
+//   else if (r == 1)
+//   {
+//     printf("*****iTH Tail *****\n");
+//     s =deq_tail_ith(q, index);
+//     printf("%s\n",s);
+//   }
+//   else {
+//     printf("An invalid option was entered for r \n");
+//     return ;
+//   }
+//   printf("*****Test complete*****\n");
 
-  }
-  else if (t == 3){
-    //insert nodes 
-    deq_tail_put(q, t1);
-    deq_tail_put(q, t2);
-    deq_tail_put(q, t3); 
-    deq_tail_put(q, t4);
-    deq_tail_put(q, t5);
-    deq_tail_put(q, t6);
-    deq_tail_put(q, t7);
+// }
 
-   deq_head_get(q); 
-  }
-  else if (t == 4){
-    //insert nodes 
-    deq_tail_put(q, t1);
-    deq_tail_put(q, t2);
-    deq_tail_put(q, t3); 
-    deq_tail_put(q, t4);
-    deq_tail_put(q, t5);
-    deq_tail_put(q, t6);
-    deq_tail_put(q, t7);
+// /**
+//  * run_test - test put get
+//  * t - test to run ( 1 = head put, 2 - tail put, 3 = tail put/get head,
+//  *    4 - tail put/get tail )
+// */
 
-    deq_tail_get(q);
-  }
-  else{
-    printf("Incorrect input, enter 1 to run head_put, enter 2 to run tail_put");
-  }
-}
+// static void run_test(Deq q, int t){
 
+//   char *t1 = "test1 ";
+//   char *t2 = "test2 ";
+//   char *t3 = "test3 ";
+//   char *t4 = "test4 ";
+//   char *t5 = "test5 ";
+//   char *t6 = "test6 ";
+//   char *t7 = "test7 ";
 
+//   if (t == 1){
+//     printf("*****PUT H *****\n");
+//     deq_head_put(q, t1);
+//     deq_head_put(q, t2);
+//     deq_head_put(q, t3);
+//     deq_head_put(q, t4);
+//     deq_head_put(q, t5);
+//     deq_head_put(q, t6);
+//     deq_head_put(q, t7);
+//   }
+//   else if (t == 2){
 
+//    printf("*****PUT T *****\n");
+//     deq_tail_put(q, t1);
+//     deq_tail_put(q, t2);
+//     deq_tail_put(q, t3);
+//     deq_tail_put(q, t4);
+//     deq_tail_put(q, t5);
+//     deq_tail_put(q, t6);
+//     deq_tail_put(q, t7);
 
+//   }
+//   else if (t == 3){
+//     //insert nodes
+//     deq_tail_put(q, t1);
+//     deq_tail_put(q, t2);
+//     deq_tail_put(q, t3);
+//     deq_tail_put(q, t4);
+//     deq_tail_put(q, t5);
+//     deq_tail_put(q, t6);
+//     deq_tail_put(q, t7);
 
+//    deq_head_get(q);
+//   }
+//   else if (t == 4){
+//     //insert nodes
+//     deq_tail_put(q, t1);
+//     deq_tail_put(q, t2);
+//     deq_tail_put(q, t3);
+//     deq_tail_put(q, t4);
+//     deq_tail_put(q, t5);
+//     deq_tail_put(q, t6);
+//     deq_tail_put(q, t7);
 
-int main() {
-  Deq q=deq_new();
+//     deq_tail_get(q);
+//   }
+//   else{
+//     printf("Incorrect input, enter 1 to run head_put, enter 2 to run tail_put");
+//   }
+// }
 
-  run_test(q,5);
-  run_ith(q,0, 0, 2);
-  run_rem(q,0,1);
-  run_rem(q,1,0);
+int main()
+{
+  Deq q = deq_new();
 
-  deq_del(q,0);
+  // run_test(q,5);
+  // run_ith(q,0, 0, 2);
+  // run_rem(q,0,1);
+  // run_rem(q,1,0);
+
+  deq_del(q, 0);
   return 0;
-
 }
